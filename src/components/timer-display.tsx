45feeaf1a69e4fb18a6994985a03558203135d39
igor <cruigor@gmail.com>
2025-12-11 12:15:51 -0300

    feat: criando notificao de termino de partida

--- src/components/timer-display.tsx
+++ src/components/timer-display.tsx
@@ -1,26 +1,29 @@
 import { Button } from "@/components/ui/button";
 import { useTimer } from "@/hooks/use-timer";
+import ShowTimeEnded from "@/utils/show-time-ended";
 import { Play, Pause, TimerReset } from "lucide-react";
 import { useCallback } from "react";
 
 const startVibration = () => {
-    if (window.navigator && window.navigator.vibrate) {
-        window.navigator.vibrate([200, 100, 200]);
-    } else {
-        alert("Jogo encerrado!");
-    }
+  if (window.navigator && window.navigator.vibrate) {
+    window.navigator.vibrate([200, 100, 200]);
+    ShowTimeEnded();
+  } else {
+    ShowTimeEnded();
+  }
 };
 
 export default function TimerDisplay() {
   const handleTimeFinish = useCallback(() => {
-        startVibration();
-    }, []);
-  const { formattedTime, isRunning, start, pause, reset } = useTimer(5000, handleTimeFinish);
-
+    startVibration();
+  }, []);
+  const { formattedTime, isRunning, start, pause, reset } = useTimer(
+    5000,
+    handleTimeFinish
+  );
 
   return (
     <div className="sticky top-25 z-10 flex items-center justify-center p-3 backdrop-blur-sm rounded-lg shadow-xl mb-4 border max-w-2xl mx-auto gap-2 bg-card/55">
-
       <div className="text-3xl text-center font-extrabold text-gray-300 tracking-wider mb-2">
         {formattedTime}
       </div>
@@ -39,7 +42,12 @@ export default function TimerDisplay() {
           )}
         </Button>
 
-        <Button onClick={reset} variant="outline" size="sm" className="cursor-pointer">
+        <Button
+          onClick={reset}
+          variant="outline"
+          size="sm"
+          className="cursor-pointer"
+        >
           <TimerReset className="w-4 h-4" />
         </Button>
       </div>
